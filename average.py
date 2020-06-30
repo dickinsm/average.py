ee6037be7282ff0f9a1cc3843e259f7923403329
Makaliah Dickinson <67165318+dickinsm@users.noreply.github.com>
2020-06-30 16:41:13 -0700

    Update average.py

--- average.py
+++ average.py
@@ -6,4 +6,5 @@ num = float(input("Enter number: "))
 num = float(input("Enter number: "))
 total_sum =+ num
 avg = total_sum/5
-print("The average of those numbers is: ", avg )
+print("The average of those numbers is:") 
+print(avg)
