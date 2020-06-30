000c98b5f9d28ecaf3daf49efde6f08d6303982a
dickinsm <dickinsonmakaliah@gmail.com>
2020-06-29 22:02:16 -0700

    Initial commit

---
+++ average.py
@@ -0,0 +1,9 @@
+input("Please enter five numbers")
+num = float(input("Enter number: "))
+num = float(input("Enter number: "))
+num = float(input("Enter number: "))
+num = float(input("Enter number: "))
+num = float(input("Enter number: "))
+total_sum =+ num
+avg = total_sum/5
+print("The average of those numbers is: ", avg )
